c3bec5be594ca2b9e996cb1779a86aa28023ba10
naphatchanun <naphatchanun.suechey@gmail.com>
2026-04-22 18:53:10 +0700

    task-002
    - สถานะ + filter
    - 1หน้า เเสดง 10 รายการ

--- app/(tab)/medicalHis/index.tsx
+++ app/(tab)/medicalHis/index.tsx
@@ -1,12 +1,19 @@
-import { useLocalSearchParams, router } from "expo-router";
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
 import BackButton from "@/components/backButton";
+import { Ionicons } from "@expo/vector-icons";
+import { router, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
+/* ------------------ MOCK โรค ------------------ */
 const historyData = [
   {
     id: 10,
-    No: 10,
     date: "3 ตุลาคม 2568",
     Note: "ผลตรวจ HbA1c ลดลงและอยู่ในเกณฑ์ที่ดี",
     status: "สีเขียว",
@@ -15,7 +22,6 @@ const historyData = [
   },
   {
     id: 9,
-    No: 9,
     date: "3 กันยายน 2568",
     Note: "แนะนำให้ปรับพฤติกรรมการรับประทานอาหาร",
     status: "สีเหลือง",
@@ -24,107 +30,239 @@ const historyData = [
   },
   {
     id: 8,
-    No: 8,
-    date: "3 กรกฎาคม 2568",
-    Note: "เพิ่มการออกกำลังกายโดยผู้ป่วยตอบรับดี",
+    date: "3 มิถุนายน 2568",
+    Note: "แนะนำให้ปรับพฤติกรรมการรับประทานอาหาร",
     status: "สีเหลือง",
     DoctorName: "นางจิต ใจดี",
     ColorStatus: "#FFD57B",
   },
+];
+
+/* ------------------ MOCK วัคซีน ------------------ */
+const vaccineData = [
   {
-    id: 7,
-    No: 7,
-    date: "3 มิถุนายน 2568",
-    Note: "ระดับน้ำตาลในเลือดยังสูงเกินเกณฑ์",
-    status: "สีแดง",
-    DoctorName: "นางจิต ใจดี",
+    id: 1,
+    vaccineType: "วัคซีนไข้หวัดใหญ่",
+    vaccineName: "Influenza Vaccine",
+    receiveDate: "1 มกราคม 2568",
+    recommendedAge: "6 เดือนขึ้นไป",
+    status: "ได้รับวัคซีนเเล้ว",
+    ColorStatus: "#58AD46",
+  },
+  {
+    id: 2,
+    vaccineType: "วัคซีนโควิด-19",
+    vaccineName: "Pfizer",
+    receiveDate: "10 กุมภาพันธ์ 2568",
+    recommendedAge: "12 ปีขึ้นไป",
+    status: "รอรับวัคซีน",
+    ColorStatus: "#FFD57B",
+  },
+  {
+    id: 3,
+    vaccineType: "วัคซีนตับอักเสบ B",
+    vaccineName: "Hepatitis B",
+    receiveDate: "เลยกำหนด",
+    recommendedAge: "แรกเกิด",
+    status: "ยังไม่ได้รับวัคซีน",
     ColorStatus: "#FF0505",
+  },
+  {
+    id: 4,
+    vaccineType: "วัคซีนไข้หวัดใหญ่",
+    vaccineName: "Influenza Vaccine",
+    receiveDate: "1 มกราคม 2568",
+    recommendedAge: "6 เดือนขึ้นไป",
+    status: "ได้รับวัคซีนเเล้ว",
+    ColorStatus: "#58AD46",
   },
 ];
 
 export default function MedicalHisPage() {
   const { disease } = useLocalSearchParams<{ disease: string }>();
-
+  const [page, setPage] = useState(0);
+  const PAGE_SIZE = 10;
+  const isVaccine = disease === "วัคซีนเด็ก";
+  const [filter, setFilter] = useState("ทั้งหมด");
+  const [open, setOpen] = useState(false);
+  const filteredVaccine = vaccineData.filter((item) => {
+    if (filter === "ทั้งหมด") return true;
+    return item.status === filter;
+  });
+  const start = page * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+  const pageData = filteredVaccine.slice(start, end);
+  const maxPage = Math.ceil(filteredVaccine.length / PAGE_SIZE);
   return (
     <View style={styles.wrapper}>
       <ScrollView
         style={styles.container}
         showsVerticalScrollIndicator={false}
       >
-        {/* Header */}
         <View style={styles.header}>
           <View style={styles.backWrapper}>
             <BackButton targetPath={`/(tab)/home`} />
           </View>
-
           <Text style={styles.title}>รายการประวัติการรักษา</Text>
           <Text style={styles.subTitle}>
             {disease || "ไม่พบข้อมูลโรค"}
           </Text>
         </View>
 
-        {/* Cards */}
-        {historyData.map((item) => (
-          <View key={item.id} style={styles.card}>
-            <View style={styles.rowBetween}>
-              <Text style={styles.visitTitle}>ครั้งที่ {item.id}</Text>
-              <Text style={styles.date}>{item.date}</Text>
-            </View>
-
-            <Text
-              style={styles.text}
-              numberOfLines={1}
-              ellipsizeMode="tail"
+        {isVaccine && (
+          <View style={{ marginBottom: 12, position: "relative", zIndex: 10 }}>
+            <TouchableOpacity
+              style={styles.dropdown}
+              onPress={() => setOpen(!open)}
             >
-              การรักษา : {item.Note}
-            </Text>
+              <Text style={styles.dropdownText}>{filter}</Text>
+              <Ionicons
+                name="chevron-down"
+                size={20}
+                color="#05548D"
+                style={{
+                  transform: [{ rotate: open ? "180deg" : "0deg" }],
+                }}
+              />
+            </TouchableOpacity>
+            {open && (
+              <View style={styles.dropdownMenu}>
+                {[
+                  "ทั้งหมด",
+                  "ได้รับวัคซีนเเล้ว",
+                  "รอรับวัคซีน",
+                  "ยังไม่ได้รับวัคซีน",
+                ].map((item) => (
+                  <TouchableOpacity
+                    key={item}
+                    style={styles.dropdownItem}
+                    onPress={() => {
+                      setFilter(item);
+                      setOpen(false);
+                    }}
+                  >
+                    <Text style={styles.optionText}>{item}</Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+            )}
+          </View>
+        )}
 
-            <View style={styles.statusRow}>
-              <Text style={styles.text}>สถานะ : </Text>
-
+        {isVaccine &&
+          pageData.map((item) => (
+            <View key={item.id} style={styles.card}>
               <View
                 style={[
                   styles.statusBadge,
+                  styles.statusTopRight,
                   { backgroundColor: item.ColorStatus },
                 ]}
               >
-                <Text style={styles.statusText}>
-                  {item.status}
+                <Text style={styles.statusText}>{item.status}</Text>
+              </View>
+              <Text style={styles.visitTitle}>{item.vaccineType}</Text>
+              <Text style={styles.text}>
+                ชื่อวัคซีน : {item.vaccineName}
+              </Text>
+              <Text style={styles.text}>
+                อายุที่ควรได้รับ : {item.recommendedAge}
+              </Text>
+              <View style={styles.bottomRow}>
+                <Text style={styles.text}>
+                  วันที่ได้รับ : {item.receiveDate}
                 </Text>
+                <TouchableOpacity
+                  style={styles.arrowButton}
+                  onPress={() =>
+                    router.push({
+                      pathname: "/(tab)/medicalHis/detail/[id]",
+                      params: {
+                        id: String(item.id),
+                        disease: String(disease),
+                      },
+                    })} >
+                  <Ionicons name="arrow-forward-circle-outline" size={24} color="#05548D" />
+                </TouchableOpacity>
               </View>
             </View>
-
-            <View style={styles.bottomRow}>
+          ))}
+        {!isVaccine &&
+          historyData.map((item) => (
+            <View key={item.id} style={styles.card}>
+              <View style={styles.rowBetween}>
+                <Text style={styles.visitTitle}>ครั้งที่ {item.id}</Text>
+                <Text style={styles.date}>{item.date}</Text>
+              </View>
               <Text style={styles.text}>
-                ผู้ตรวจ : {item.DoctorName}
+                การรักษา : {item.Note}
               </Text>
-
-              <TouchableOpacity
-                style={styles.arrowBtn}
-                onPress={() =>
-                  router.push({
-                    pathname: "/(tab)/medicalHis/detail/[id]",
-                    params: {
-                      id: String(item.id),
-                      disease: String(disease),
-                    },
-                  })
-                }
-              >
-                <Ionicons
-                  name="arrow-forward-circle-outline"
-                  size={24}
-                  color="#05548D"
-                />
-              </TouchableOpacity>
+              <View style={styles.statusRow}>
+                <Text style={styles.text}>สถานะ : </Text>
+                <View
+                  style={[
+                    styles.statusBadge,
+                    { backgroundColor: item.ColorStatus },
+                  ]}
+                >
+                  <Text style={styles.statusText}>{item.status}</Text>
+                </View>
+              </View>
+              <View style={styles.bottomRow}>
+                <Text style={styles.text}> ผู้ตรวจ : {item.DoctorName} </Text>
+                <TouchableOpacity
+                  style={styles.arrowButton}
+                  onPress={() =>
+                    router.push({
+                      pathname: "/(tab)/medicalHis/detail/[id]",
+                      params: {
+                        id: String(item.id),
+                        disease: String(disease),
+                      },
+                    })} >
+                  <Ionicons name="arrow-forward-circle-outline" size={24} color="#05548D" />
+                </TouchableOpacity>
+              </View>
             </View>
+          ))}
+        {isVaccine && (
+          <View style={styles.pagination}>
+            <TouchableOpacity
+              disabled={page === 0}
+              onPress={() => setPage(page - 1)}
+              style={[styles.pageButton, page === 0 && { opacity: 0.4 }]}
+            >
+              <View style={styles.buttonContent}>
+                <Ionicons name="caret-back-outline" size={22} color="#05548D" />
+                <Text style={styles.navText}>ย้อนกลับ</Text>
+              </View>
+            </TouchableOpacity>
+
+            <Text style={styles.text}>
+              {page + 1} / {maxPage}
+            </Text>
+
+            <TouchableOpacity
+              disabled={page + 1 >= maxPage}
+              onPress={() => setPage(page + 1)}
+              style={[
+                styles.pageButton,
+                page + 1 >= maxPage && { opacity: 0.4 },
+              ]}
+            >
+              <View style={styles.buttonContent}>
+                <Text style={styles.navText}>นัดถัดไป</Text>
+                <Ionicons name="caret-forward-outline" size={22} color="#05548D" />
+              </View>
+            </TouchableOpacity>
           </View>
-        ))}
+        )}
       </ScrollView>
     </View>
   );
 }
 
+/* ------------------ STYLE ------------------ */
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
@@ -133,15 +271,12 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: "#EBF7FF",
     paddingHorizontal: 16,
-    paddingTop: 8,
   },
 
   header: {
     paddingTop: 16,
     paddingBottom: 20,
-    justifyContent: "center",
     alignItems: "center",
     position: "relative",
   },
@@ -150,38 +285,35 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: 0,
     top: 18,
-    zIndex: 10,
   },
 
   title: {
     fontSize: 22,
     fontFamily: "IBMPlexSansThai_700Bold",
-    textAlign: "center",
   },
 
   subTitle: {
     fontSize: 22,
     fontFamily: "IBMPlexSansThai_700Bold",
-    textAlign: "center",
-    marginTop: 2,
   },
 
   card: {
-    backgroundColor: "#FFFFFF",
+    backgroundColor: "#fff",
     borderRadius: 16,
     padding: 16,
     marginBottom: 12,
-  },
-
-  rowBetween: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    position: "relative",
   },
 
   visitTitle: {
-    fontSize: 18,
-    fontFamily: "IBMPlexSansThai_700Bold",
+    fontSize: 20,
+    fontFamily: "IBMPlexSansThai_600SemiBold",
+  },
+
+  text: {
+    fontSize: 16,
+    fontFamily: "IBMPlexSansThai_500Medium",
+    marginTop: 8,
   },
 
   date: {
@@ -189,10 +321,9 @@ const styles = StyleSheet.create({
     fontFamily: "IBMPlexSansThai_500Medium",
   },
 
-  text: {
-    fontSize: 15,
-    fontFamily: "IBMPlexSansThai_500Medium",
-    marginTop: 8,
+  rowBetween: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 
   statusRow: {
@@ -203,24 +334,92 @@ const styles = StyleSheet.create({
 
   statusBadge: {
     paddingHorizontal: 10,
-    paddingVertical: 2,
-    borderRadius: 8,
+    paddingVertical: 4,
+    borderRadius: 12,
+  },
+
+  statusTopRight: {
+    position: "absolute",
+    top: 12,
+    right: 10,
   },
 
   statusText: {
     color: "#fff",
-    fontSize: 14,
-    fontFamily: "IBMPlexSansThai_600SemiBold",
   },
 
   bottomRow: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center",
     marginTop: 6,
+    alignItems: "center",
   },
 
-  arrowBtn: {
-    marginLeft: 8,
+  dropdown: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 12,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "#05548D",
+    marginBottom: 8,
+  },
+
+  dropdownText: {
+    fontSize: 16,
+    fontFamily: "IBMPlexSansThai_500Medium",
+  },
+
+  dropdownMenu: {
+    position: "absolute",
+    top: 55,
+    left: 0,
+    right: 0,
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    zIndex: 999,
+    elevation: 5,
+  },
+
+  dropdownItem: {
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+  },
+  optionText: {
+    fontSize: 16,
+    fontFamily: "IBMPlexSansThai_500Medium",
+  },
+  arrowButton: { 
+    marginLeft: 8, 
+  },
+
+  pagination: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+
+  pageButton: {
+    padding: 10,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  navText: {
+    fontSize: 18,
+    color: "#05548D",
+    fontFamily: "IBMPlexSansThai_700Bold",
+  },
+  buttonContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 6,
   },
 });
