c64c904b45c029802dcebca3098a97338d711c15
naphatchanun <naphatchanun.suechey@gmail.com>
2026-04-28 21:46:18 +0700

    เเก้ shadow + ขนาด dropdown

--- app/(tab)/medicalHis/index.tsx
+++ app/(tab)/medicalHis/index.tsx
@@ -377,12 +377,17 @@ const styles = StyleSheet.create({
     top: 55,
     left: 0,
     right: 0,
+    width: 150,
     backgroundColor: "#fff",
     borderRadius: 12,
     borderWidth: 1,
     borderColor: "#ddd",
     zIndex: 999,
-    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.15,
+    shadowRadius: 8,
+    elevation: 8,
   },
 
   dropdownItem: {
