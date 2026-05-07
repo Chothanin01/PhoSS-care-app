001d680f387d5cf34f483d075b78f5bcbcac90aa
ChothaninThitisrirat <maithong31.mmm@gmail.com>
2026-05-08 00:20:59 +0700

    deploy setting

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080", 
+  baseURL: process.env.EXPO_PUBLIC_API_URL, 
 });
 
 let isRefreshing = false;
